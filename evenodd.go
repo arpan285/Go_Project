00745e39d97f049b8b13862e82a2147626233b9f
Arpan Chakraborty <arpanchakraborty451@gmail.com>
2022-03-26 01:58:37 +0530

    comitting a go file

---
+++ evenodd.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int
+
+	fmt.Printf("Enter a nuber: ")
+	fmt.Scanf("%v", &a)
+	//fmt.Println("Your numbers is:", a)
+
+	for i := 0; i < a; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%v is even\n", i)
+		} else {
+			fmt.Printf("%v is odd\n", i)
+		}
+	}
+
+}
