55b50c90627668d5ffdd0f2538bd1269f2b7eef5
Your Name <you@example.com>
2022-03-31 02:21:27 +0530

    create a file

--- evenodd.go
+++ evenodd.go
@@ -8,14 +8,19 @@ func main() {
 
 	fmt.Printf("Enter a nuber: ")
 	fmt.Scanf("%v", &a)
-	//fmt.Println("Your numbers is:", a)
+	fmt.Println("Your numbers is:", a)
 
-	for i := 0; i < a; i++ {
+	/*for i := 0; i < a; i++ {
 		if i%2 == 0 {
 			fmt.Printf("%v is even\n", i)
 		} else {
 			fmt.Printf("%v is odd\n", i)
 		}
+	}*/
+	if a%2 == 0 {
+		fmt.Printf("%v is even\n", a)
+	} else {
+		fmt.Printf("%v is odd\n", a)
 	}
 
 }
